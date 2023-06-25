163ecbbe871af32bf839bc0750bc963a325acdcd
ya_kaka <70700353+Flusak@users.noreply.github.com>
2023-06-25 13:33:38 +0300

    Add files via upload

---
+++ ssh-sendkey/sshconfig.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ $EUID -ne 0 ]
+then
+echo Error: script not running by sudo
+exit
+fi
+
+read -p "New port: " port
+
+sed -i -e "s/#Port 22/Port $port/; s/#PermitRootLogin yes/PermitRootLogin no/;\
+ s/#PubkeyAuthentication yes/PubkeyAuthentication yes/;\
+ s/PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/sshd_config
+
+firewall-cmd --permanent --zone=public --add-port=$port/tcp
+firewall-cmd --reload
+
+yum install -y policycoreutils-python
+semanage port -a -t ssh_port_t -p tcp $port
+
+systemctl restart sshd
