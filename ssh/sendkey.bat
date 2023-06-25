c2dfdf2daa20328970b741cd49855aa7fb0d8cf5
ya_kaka <70700353+Flusak@users.noreply.github.com>
2023-06-25 13:30:10 +0300

    Create sendkey.bat

---
+++ ssh/sendkey.bat
@@ -0,0 +1,47 @@
+@echo off
+
+set key=%2
+set keydef=%userprofile%\.ssh\id_rsa.pub
+
+if "%1"=="-i" ( 
+    goto checkey
+    ) else (
+        goto checkdef
+    )
+goto:eof
+
+:checkey
+echo %key% | findstr ".pub" >nul
+if errorlevel 1 (
+    @echo Error: this is not a public key!
+    exit
+) else (
+    type %key% 2>nul >nul
+    if errorlevel 1 (
+        @echo Error: key %key% not found.
+        exit
+    ) else (
+        set usekey=%key%
+        goto sshcopykey
+    )
+)
+goto:eof
+
+:checkdef
+@type %keydef% 2>nul >nul
+if errorlevel 1 (
+    @echo Error: id_rsa.pub not found!
+) else (
+    set usekey=%keydef%
+    goto sshcopykey
+)
+goto:eof
+
+:sshcopykey
+set /P user="User: "
+set /P host="Host: "
+type %usekey% | ssh %user%@%host% "mkdir -p ~/.ssh && cat >> ~/.ssh/authorized_keys && chmod 0700 ~/.ssh && chmod 0644 ~/.ssh/authorized_keys"
+
+scp -i %usekey:.pub=% ./sshconfig.sh %user%@%host%:.
+
+ssh -t %user%@%host% "sudo bash sshconfig.sh"
