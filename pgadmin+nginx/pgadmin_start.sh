ff9747028843f88c601f88852dc18184f73c18dd
ya_kaka <70700353+Flusak@users.noreply.github.com>
2023-06-30 20:30:41 +0300

    Update pgadmin_start.sh

--- pgadmin+nginx/pgadmin_start.sh
+++ pgadmin+nginx/pgadmin_start.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
 if [ $EUID -ne 0 ]
 then
 echo Error: script not running by sudo
 exit
 fi
+
 
 if ! [ -d ./pgadmin4 ]
 then
@@ -10,39 +12,39 @@ echo 'Can`t find a directory! Have you run a installation script?'
 exit
 fi
 
-nohup ./pgadmin4/bin/python3 ./pgadmin4/lib/python3.6/site-packages/pgadmin4/pgAdmin4.py  &
-
-sleep 5 &&
+for i in $(ps -aux | egrep "pgadmin4" | tr -s "\t " " "| cut -f2 -d' ')
+do
+		kill -9 $i > /dev/null 2> /dev/null
+done
 
 green='\033[0;32m' &&
 red='\033[0;31m' &&
 endcolor='\033[0m' &&
-num_try=0 &&
+num_try=0
 
-info=$(curl -I -o /dev/stdout --url http://localhost:80/ -s)
-echo "Try to check \$ip_con:80" &&
+nohup ./pgadmin4/bin/python3 ./pgadmin4/lib/python3.6/site-packages/pgadmin4/pgAdmin4.py  &
+sleep 5 &&
+
+info=$(curl -I -o /dev/stdout --url http://localhost:80/ -s) &&
 while ! echo $info | grep -E ".*login\?.*" > /dev/null
 do
-    ((num_try+=1))
-    if [ "$num_try" -ge "4" ]
-    then
-        echo -e "${red}Something went wrong... Try to run installation script again!${endcolor}"
-        exit
-    fi
-    echo "$num_try: Failed. Trying to start pgadmin again..."
-    for i in $(ps -aux | egrep "pgadmin4" | tr -s "\t " " "| cut -f2 -d' ')
-    do
-        kill -9 $i
-        nohup ./pgadmin4/bin/python3 ./pgadmin4/lib/python3.6/site-packages/pgadmin4/pgAdmin4.py  &
-        sleep 7
-    done
+	((num_try+=1))
+    time_sleep=$((5 + num_try))
+	echo "$num_try: Failed. Trying again in $time_sleep seconds..."
+	for i in $(ps -aux | egrep "pgadmin4" | tr -s "\t " " "| cut -f2 -d' ')
+	do
+		kill -9 $i > /dev/null 2> /dev/null
+	done
+	nohup ./pgadmin4/bin/python3 ./pgadmin4/lib/python3.6/site-packages/pgadmin4/pgAdmin4.py  &
+	sleep $time_sleep
+    info=$(curl -I -o /dev/stdout --url http://localhost:80/ -s)
 done
 
 if echo $info | grep -E ".*login\?.*" > /dev/null
 then
-    echo -e "${green}pgadmin is running!${endcolor}"
-    exit
+    echo -e "${green}pgAdmin is running!${endcolor}"
+	exit
 else
-    echo -e "${red}Something went wrong... Try to run installation script again!${endcolor}"
+	echo -e "${red}Something went wrong...${endcolor}"
     exit
 fi
