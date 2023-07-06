0a1ce60377db90e1d0c80b4ec1df7ab2bdc70e7a
ya_kaka <70700353+Flusak@users.noreply.github.com>
2023-07-06 21:46:21 +0300

    Update pgadmin_start.sh

--- pgadmin+nginx/vol/pgadmin_start.sh
+++ pgadmin+nginx/vol/pgadmin_start.sh
@@ -12,9 +12,10 @@ echo 'Can`t find a directory! Have you run a installation script?'
 exit
 fi
 
+
 for i in $(ps -aux | egrep "pgadmin4" | tr -s "\t " " "| cut -f2 -d' ')
 do
-		kill -9 $i > /dev/null 2> /dev/null
+                kill -9 $i > /dev/null 2> /dev/null
 done
 
 green='\033[0;32m' &&
@@ -22,29 +23,28 @@ red='\033[0;31m' &&
 endcolor='\033[0m' &&
 num_try=0
 
+sudo kill nginx
+nginx
 nohup ./pgadmin4/bin/python3 ./pgadmin4/lib/python3.6/site-packages/pgadmin4/pgAdmin4.py  &
 sleep 5 &&
 
 info=$(curl -I -o /dev/stdout --url http://localhost:80/ -s) &&
 while ! echo $info | grep -E ".*login\?.*" > /dev/null
 do
-	((num_try+=1))
+        ((num_try+=1))
     time_sleep=$((5 + num_try))
-	echo "$num_try: Failed. Trying again in $time_sleep seconds..."
-	for i in $(ps -aux | egrep "pgadmin4" | tr -s "\t " " "| cut -f2 -d' ')
-	do
-		kill -9 $i > /dev/null 2> /dev/null
-	done
-	nohup ./pgadmin4/bin/python3 ./pgadmin4/lib/python3.6/site-packages/pgadmin4/pgAdmin4.py  &
-	sleep $time_sleep
+        echo "$num_try: Failed. Trying again in $time_sleep seconds..."
+        for i in $(ps -aux | egrep "pgadmin4" | tr -s "\t " " "| cut -f2 -d' ')
+        do
+                kill -9 $i > /dev/null 2> /dev/null
+        done
+        sudo kill nginx
+        nginx
+        nohup ./pgadmin4/bin/python3 ./pgadmin4/lib/python3.6/site-packages/pgadmin4/pgAdmin4.py  &
+        sleep $time_sleep
     info=$(curl -I -o /dev/stdout --url http://localhost:80/ -s)
 done
 
 if echo $info | grep -E ".*login\?.*" > /dev/null
 then
     echo -e "${green}pgAdmin is running!${endcolor}"
-	exit
-else
-	echo -e "${red}Something went wrong...${endcolor}"
-    exit
-fi
